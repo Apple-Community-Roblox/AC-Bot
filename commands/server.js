ff316ac30306318ab9eae97bea77d4bc37a91355
joshuadl12 <joshuadeleon210@gmail.com>
2019-09-27 20:24:39 -0500

    FIXED :D OMG

--- commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'Gives server info',
-	execute(client,message, args) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	},
-  };
