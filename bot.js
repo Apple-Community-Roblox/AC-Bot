802606d08678ca2eb7280c819e0ba809191e9abd
joshuadl12 <joshuadeleon210@gmail.com>
2019-10-14 17:19:13 +0200

    fix

--- bot.js
+++ bot.js
@@ -178,17 +178,17 @@ client.on('message', async (message) => {
       if (member.roles.some(role => role.name === 'AC Mods', 'AC Admins')) {
         let reason = args.slice(1).join(' ');
         let user = message.mentions.users.first();
-        let modlog = client.channels.find('name', 'mod-log');
+        let modlog = client.channels.find('name', 'logs');
         if (!modlog) return message.reply('I cannot find a mod-log channel');
         if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
         if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
-        const embed = new Discord.RichEmbed()
+        const WarnEmbed = new Discord.RichEmbed()
         .setColor(0x00AE86)
         .setTimestamp()
         .addField('Action:', 'Warning')
         .addField('User:', `${user.username}#${user.discriminator}`)
         .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`);
-        return client.channels.get(modlog.id).sendEmbed(embed)
+        return client.channels.get(modlog.id).sendEmbed(WarnEmbed)
       } else {
         message.reply('Oops! Incorrect permissions');
       }
